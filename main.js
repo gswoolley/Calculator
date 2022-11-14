49643fb9ac8addda9bacc986a1fef325c8c29949
Garett <g.woolley02@gmail.com>
2022-11-14 11:19:43 -0500

    Calc working

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const display = document.getElementById("screen");
 const one = document.getElementById("1");
 const two = document.getElementById("2");
 const three = document.getElementById("3");
@@ -7,8 +8,15 @@ const six = document.getElementById("6");
 const seven = document.getElementById("7");
 const eight = document.getElementById("8");
 const nine = document.getElementById("9");
-const display = document.getElementById("screen");
+const addBtn = document.getElementById("+");
+const subtractBtn = document.getElementById("-");
+const multiplyBtn = document.getElementById("*");
+const divideBtn = document.getElementById("/");
+const equalsBtn = document.getElementById("=");
+const clearBtn = document.getElementById("clear");
 let displayValue = "";
+let oldDisplay = "";
+let operator = "";
 
 //The basic math functions
 function add(a, b) {
@@ -42,6 +50,7 @@ function operate(operator, a, b) {
     return divide(a, b);
   }
 }
+
 //DOM methods:
 
 one.onclick = () => {
@@ -66,4 +75,77 @@ four.onclick = () => {
   display.value += "4";
   displayValue += "4";
   console.log(displayValue);
+};
+
+five.onclick = () => {
+  display.value += "5";
+  displayValue += "5";
+  console.log(displayValue);
+};
+
+six.onclick = () => {
+  display.value += "6";
+  displayValue += "6";
+  console.log(displayValue);
+};
+
+seven.onclick = () => {
+  display.value += "7";
+  displayValue += "7";
+  console.log(displayValue);
+};
+
+eight.onclick = () => {
+  display.value += "8";
+  displayValue += "8";
+  console.log(displayValue);
+};
+
+nine.onclick = () => {
+  display.value += "9";
+  displayValue += "9";
+  console.log(displayValue);
+};
+
+clearBtn.onclick = () => {
+  displayValue = "";
+  display.value = "";
+  oldDisplay = "";
+};
+
+addBtn.onclick = () => {
+  operator = "+";
+  console.log(operator);
+  oldDisplay = displayValue;
+  displayValue = "";
+  display.value = "";
+};
+
+subtractBtn.onclick = () => {
+  operator = "-";
+  console.log(operator);
+  oldDisplay = displayValue;
+  displayValue = "";
+  display.value = "";
+};
+multiplyBtn.onclick = () => {
+  operator = "*";
+  console.log(operator);
+  oldDisplay = displayValue;
+  displayValue = "";
+  display.value = "";
+};
+divideBtn.onclick = () => {
+  operator = "/";
+  console.log(operator);
+  oldDisplay = displayValue;
+  displayValue = "";
+  display.value = "";
+};
+
+equalsBtn.onclick = () => {
+  console.log(oldDisplay, displayValue, operator);
+  console.log(operate(operator, +oldDisplay, +displayValue));
+  display.value = operate(operator, +oldDisplay, +displayValue);
+  displayValue = operate(operator, +oldDisplay, +displayValue);
 };
